7166f0a48e81e29e1ea3527b35385aee15a02b6c
吕云鹏 <391885904@qq.com>
2020-10-18 18:21:11 +0800

    yunpeng

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,13 @@
-const inject
+const inject = (items,sections) => {
+  let [section1,section2] = sections;
+  let {"content":item1,"index":index1} = section1;
+  let {"content":item2,"index":index2} = section2;
+
+  items.splice(index1,0,item1);
+  items.splice(3,0,item2);
+
+  console.log(index1,index2);
+
+  return items;
+}
 export { inject };
